a5964d8ce367b6dc66a65c03fe0e4ee154381459
Sascha <124897282+5aschaB@users.noreply.github.com>
2023-02-22 10:34:57 +0000

    Add files via upload

---
+++ risk_matrix_1.2.py
@@ -0,0 +1,116 @@
+#__author__ = "Sascha Bharath"
+#__version__ = "1.2"
+#__project__ = "CS261 Group Project"
+#__status__ = "Production"
+
+import math
+
+################### METRIC CLASSES ###################
+
+## Parent class of all metrics
+class Metric:
+  def __init__(self, name, projectid, priority, score):
+    self.name = name
+    self.projectid = projectid
+    self.priority = priority
+    self.score = score
+    self.trend = 0
+    self.islow = False
+
+
+class SoftMetric(Metric):
+  def __init__():
+      pass
+
+    
+class HardMetric(Metric):
+  def __init__():
+      pass
+
+################### METRIC FUNCTIONS ###################
+
+## function that calculates the score and creates an object for a soft metric
+def recordSoftMetric(projectid, metric, scorelist):
+    ## calculate the average of the survey scores
+    new_score = sum(scorelist)/len(scorelist)
+    ## the most recent timeframes, the last 20% of those recorded
+    n = math.ceil(numberOfTimeframes * 0.2)
+
+    ## check if the score has been decreasing over recent timeframes
+    score_decrease = scoreTrend(projectid, metric, n)
+    ## check if the score has been low (<=2) over recent timeframes
+    score_low = scoreLow(projectid, metric, n)
+
+    ## if the score was decreasing or low, subtract bias constant from the score
+    if ((score_decrease<0) or score_low):
+        new_score -= 1
+
+    ## if the metric is a priority, multiply by bias constant
+    priority = isPriority(projectid, metric)
+    if priority:
+        newscore *=0.9
+
+    ## round the score to the nearest integer from 1 to 10
+    newscore = round(newscore)
+    if newscore<1:
+        newscore = 1
+    if newscore>10:
+        newscore = 10
+        
+    ## create a soft metric object then return it
+    s_metric = SoftMetric(metric, projectid, priority)
+    return s_metric
+
+
+## function that calculates the score and creates an object for a soft metric
+def record_hard_metric(projectid, metric):
+    if metric=="budget":
+        ## Josh's gantt data?
+        pass
+    else if metric="code":
+        pass
+    else if metric=="time":
+        ## Josh's gantt data?
+        pass
+    else:
+        pass
+
+    ## if the metric is a priority, multiply by bias constant
+    if isPriority(projectid, metric):
+        newscore *=0.9
+
+    ## round the score to the nearest integer from 1 to 5
+    newscore = round(newscore)
+    if newscore<1:
+        newscore = 1
+    if newscore>5:
+        newscore = 5
+
+    ## create a hard metric object then return it
+    h_metric = HardMetric(metric, newscore, projectid, priority)
+    return h_metric
+
+
+## returns a boolean as to whether the metric is a priority for that project
+def isPriority((projectid, metric)):
+    pass
+    ## perform SQL query on database to determine if the metric is a priority
+    ## discuss with Rishi, there is no way to currently record priority
+
+## returns an integer as to whether the metric score has had an upwards or downwards trend
+## or has stayed the same for the last n timeframes of the project
+def scoreTrend(projectid, metric, n):
+    pass
+    ## perform SQL query on database
+    
+
+## returns a boolean as to whether the score was low for the last n timeframes of the project
+def scoreLow(projectid, metric, n):
+    pass
+    ## perform SQL query on database
+
+## returns the total number of timeframes that have been recorded for this project so far
+def numberOfTimeframes(projectid):
+    pass
+
+
