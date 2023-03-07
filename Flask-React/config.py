25b4150e5d91bcf2999192d8717fc2e6a3bb517a
Rishihan Sanmuganathan <69389269+rishihan-vs@users.noreply.github.com>
2023-03-07 16:51:22 +0000

    Add files via upload

---
+++ Flask-React/config.py
@@ -0,0 +1,49 @@
+from decouple import config
+
+DATABASE_URI = config("DATABASE_URL")
+if DATABASE_URI.startswith("postgres://"):
+    DATABASE_URI = DATABASE_URI.replace("postgres://", "postgresql://", 1)
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = config("SECRET_KEY", default="guess-me")
+    SECURITY_PASSWORD_SALT = config("SECURITY_PASSWORD_SALT", default="very-important")
+    SQLALCHEMY_DATABASE_URI = DATABASE_URI
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    BCRYPT_LOG_ROUNDS = 13
+    WTF_CSRF_ENABLED = True
+    DEBUG_TB_ENABLED = False
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
+
+    # Mail Settings
+    MAIL_DEFAULT_SENDER = "noreply@riskassessment.com"
+    MAIL_SERVER = 'sandbox.smtp.mailtrap.io'
+    MAIL_PORT = 2525
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+    MAIL_DEBUG = False
+    MAIL_USERNAME = '2cf052b668ab1a'
+    MAIL_PASSWORD = '8f041f080f3152'
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+    WTF_CSRF_ENABLED = False
+    DEBUG_TB_ENABLED = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///testdb.sqlite"
+    BCRYPT_LOG_ROUNDS = 1
+    WTF_CSRF_ENABLED = False
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    DEBUG_TB_ENABLED = False
