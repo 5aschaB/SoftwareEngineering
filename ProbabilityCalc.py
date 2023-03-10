0863c9d3c70f2df395293e89b5f07c90614bcf7b
malcs286 <125565583+malcs286@users.noreply.github.com>
2023-03-10 12:11:53 +0000

    updated comments

--- ProbabilityCalc.py
+++ ProbabilityCalc.py
@@ -48,7 +48,7 @@ def fMoraleWellbeing(metric, values): # serves as the fuzzy value generator for 
         if (metric == 10): return values[4] # if metric score is 10 (maximum), output most positive fuzzy value
         else: return values[metric//2]      # otherwise, output a fuzzy value in line with the expressed formula
     except:
-        print("Metric value out of range")  # if the prior code generates invalid input, then the zmetric data is unsuitable
+        print("Metric value out of range")  # if the prior code generates invalid input, then the metric data is unsuitable
         return(0)
 
 def fAbsences(metric, teamMembers, values): # takes week's absences, current number of team mambers and fuzzy values as inputs
@@ -63,21 +63,29 @@ def fAbsences(metric, teamMembers, values): # takes week's absences, current num
 def mean(value, deadline): #finds mean of a normal distribution for a metric, takes a fuzzy value and subprocess deadline as inputs
     z = round(st.norm.ppf(value), 5)              # finds z-value of value to 5 d.p.
     coeff1 = (-2.32635 / z)                       # -2.32635 is the z-value corresponding to the 1st percentile in the standard normal distribution
-    coeff2 = (coeff1 * deadline)                  # formation of coefficients coeff1 and coeff2 explained in documentation
-    return(coeff2 / (coeff1 - 1))                 # reasoning for this computing mean also explained in documentation
+                                                  # coeff1 = z-value for 1st percentile divided by z-value for input percentile
+                                                  # this is necessary for solving simultaneouusly for two z-values:
+                                                    # z1 = (x1 - μ)/σ, z2 = (x2 - μ)/σ, where μ is the mean, and σ is the standard deviation
+                                                    # we can then say that z1 * σ = (x1 - μ), and z2 * σ = (x2 - μ)
+                                                    # it then follows that (z2 / z1) * (x1 - μ) = (x2 - μ)
+                                                  # coeff1 is equivalent to (z2 / z1) here
+                                                    # in this mean calculation, x2 = 0, since the raw value at this 1st percentile is 0 (0 weeks since beginning of subprocess)
+    coeff2 = (coeff1 * deadline)                    # therefore from the prior equations, (coeff1 * x1) + (coeff1 * μ) = -μ
+                                                  # coeff2 is equivalent to (coeff1 * x1) here, where x1 is the raw deadline value
+    return(coeff2 / (coeff1 - 1))                 # lastly, and following on from the above reasoning, μ = coeff2 / (coeff1 - 1)
 
 def standardDev(value, deadline, mean): # finds standard deviation of a normal distribution for a metric, takes fuzzy value, subprocess deadline and mean as inputs
-    return((deadline - mean)/(round(st.norm.ppf(value), 5))) # reasoning for this computing standard deviation explained in documentation
+    return((deadline - mean)/(round(st.norm.ppf(value), 5))) # z = (x - μ)/σ, so σ = (x - μ)/z
 
 def rawValueToZ(rawValue, mean, sd): # finds z-value associated with a raw value (i.e. time value along the x-axis of a normal distribution), given the standard deviation and mean, to 5 d.p.
-    return(round(((rawValue - mean) / sd), 5)) # reasoning for this computing z-value explained in documentation
+    return(round(((rawValue - mean) / sd), 5)) # z = (x - μ)/σ
 
 def reconcileStats(metricStats, weights): # reconciles summary statistics from multiple normal distributions, takes array of metric atats and array of weightings
     m, sd = 0                                                                 # initialises variables to hold mean and standard deviation
     for i in len(metricStats):
         m += (weights[i] * (1/(sum(weights))) * metricStats[i][0])            # adds weighted mean to m
         sd += (weights[i] * ((1/(sum(weights)))**2) * (metricStats[i][1]**2)) # adds weighted variance to sd
-    sd = sqrt(sd)                                                             # finds square root of sd (weighted variances), result is weighted standard deviation
+    sd = sqrt(sd)                                                             # weighted standard deviation found from square root of variance                                                         # finds square root of sd (weighted variances), result is weighted standard deviation
     return([m, sd])
 
 def probabilityDict(deadline, mean, sd): # forms a dictionary of cumulative probabilities from week 0 to week (deadline * 4)
@@ -144,10 +152,10 @@ def reconcileMetrics(startDate, projectDeadline, subprocessDuration, totalBudget
 
 def cycleProbability(subprocesses, startDate, projectDeadline, subprocessDuration, totalBudget, budgetUsed, commits, startingTeamSize, teamSize, morale, wellbeing, absences, priorities):
     tenWeekProb = 1
-    for i in subprocesses:
+    for i in subprocesses: # for every subprocess, generate a subprocess dictionary
         subprocessDict = reconcileMetrics(startDate, projectDeadline, subprocessDuration, totalBudget, budgetUsed, commits, startingTeamSize, teamSize, morale, wellbeing, absences, priorities)
         subprocessProb = 0
-        for j in range(9):
+        for j in range(9): # aggregate probabilities from first 10weeks to generate a 10 week probability of completion
             subprocessProb += subprocessDict[j+1]
         tenWeekProb *= subprocessProb
     return(tenWeekProb)
@@ -162,6 +170,3 @@ def ganttProbabilities(projectID, cyclesMinusRepeatProb, startDate, projectDeadl
         cycles.append(cycle(i.getID(), i.getBaseCost(), i.getPredecessors(), i.getSubprocesses(), cycleProb))
 
     return(ganttChart(projectID, cycles))
-
-# Only pass to Gantt Chart if not complete yet
-# pass to ganttChart, subprocessClass and cycle
